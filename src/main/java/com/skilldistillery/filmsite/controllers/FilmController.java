8df17baa630541cd64887aeff28aeed6b6852659
aveigel <alexaveigel@yahoo.com>
2019-05-05 12:50:15 -0600

    fixed controller

--- src/main/java/com/skilldistillery/filmsite/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/filmsite/controllers/FilmController.java
@@ -17,147 +17,149 @@ import com.skilldistillery.filmsite.database.DatabaseAccessorObject;
 @Controller
 public class FilmController {
 	DatabaseAccessorObject db = new DatabaseAccessorObject();
-	
-	@RequestMapping(path="findFilm.do", params="id", method=RequestMethod.GET)
+
+	@RequestMapping(path = "findFilm.do", params = "id", method = RequestMethod.GET)
 	public ModelAndView findFilmById(String id) {
 		ModelAndView mv = new ModelAndView();
 		Film film = null;
 		try {
 			int Id = Integer.parseInt(id);
 			film = db.findFilmById(Id);
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if(film == null) {
+		if (film == null) {
 //			mv.addObject("details", film);
 //			mv.setViewName("/views/result.jsp");
 			mv.setViewName("/views/error.jsp");
-		}else {
-		mv.addObject("details", film);
-		mv.setViewName("/views/result.jsp");
+		} else {
+			mv.addObject("details", film);
+			mv.setViewName("/views/result.jsp");
 		}
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(path="addFilm.do", 
-			method=RequestMethod.POST)
+
+	@RequestMapping(path = "addFilm.do", method = RequestMethod.POST)
 	public ModelAndView addFilm(@ModelAttribute("film") @Valid Film film, Errors errors) {
 		ModelAndView mv = new ModelAndView();
 		String sf = film.getSpecial_features();
 		sf = sf.replace("none", "");
 		film.setSpecial_features(sf);
-		
-		Film createdFilm = db.createFilm(film);
-		
-		if(errors.hasErrors()) {
+
+
+		if (errors.hasErrors()) {
 			mv.addObject("value", "add");
-		    mv.setViewName("/views/modify.jsp");
-		    return mv;
-		  }
-		
-		if(createdFilm == null) {
-			mv.setViewName("/views/error.jsp");
-		}else {
-			mv.addObject("details", createdFilm);
-			mv.setViewName("/views/result.jsp");
+			mv.setViewName("/views/modify.jsp");
+			return mv;
+		} else {
+			Film createdFilm = db.createFilm(film);
+
+			if (createdFilm == null) {
+				mv.setViewName("/views/error.jsp");
+			} else {
+				mv.addObject("details", createdFilm);
+				mv.setViewName("/views/result.jsp");
+			}
 		}
-		
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(path="deleteFilm.do", method=RequestMethod.POST)
+
+	@RequestMapping(path = "deleteFilm.do", method = RequestMethod.POST)
 	public ModelAndView deleteFilm(int filmId) {
 		ModelAndView mv = new ModelAndView();
 		Film deletedFilm = db.deleteFilm(filmId);
-		
-		if(deletedFilm == null) {
+
+		if (deletedFilm == null) {
 			mv.setViewName("/views/error.jsp");
-		}else {
+		} else {
 			mv.addObject("deleted", deletedFilm);
 			mv.setViewName("/views/home.jsp");
 		}
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(path="modify.do", method=RequestMethod.GET) 
+
+	@RequestMapping(path = "modify.do", method = RequestMethod.GET)
 	public ModelAndView changeViews() {
 		ModelAndView mv = new ModelAndView();
-		
+
 		mv.setViewName("/views/modify.jsp");
 		return mv;
 	}
-	
-	@RequestMapping(path="selectMod.do", method=RequestMethod.GET) 
+
+	@RequestMapping(path = "selectMod.do", method = RequestMethod.GET)
 	public ModelAndView changeView(String value, String filmId) {
 		ModelAndView mv = new ModelAndView();
-		System.out.println("value: " + value +"/nId; " + filmId);
-		if(value.equals("change")) {
-			mv.addObject("value",value);
+		System.out.println("value: " + value + "/nId; " + filmId);
+		if (value.equals("change")) {
+			mv.addObject("value", value);
 			Film film = null;
 			try {
 				int id = Integer.parseInt(filmId);
 				film = db.findFilmById(id);
-				
-			}catch(Exception e) {}
-			if(film == null) {
+
+			} catch (Exception e) {
+			}
+			if (film == null) {
 //				System.out.println("The film is null");
 				mv.addObject("film", new Film());
 				mv.setViewName("/views/error.jsp");
-			}else {
-			mv.addObject("film", new Film());
-			mv.addObject("details", film);
-			mv.setViewName("/views/modify.jsp");
+			} else {
+				mv.addObject("film", new Film());
+				mv.addObject("details", film);
+				mv.setViewName("/views/modify.jsp");
 			}
-			
-		}else if(value.equals("delete")) {
-			mv.addObject("value",value);
+
+		} else if (value.equals("delete")) {
+			mv.addObject("value", value);
 			mv.setViewName("/views/modify.jsp");
-		}else if(value.equals("add")) {
+		} else if (value.equals("add")) {
 			mv.addObject("film", new Film());
-			mv.addObject("value",value);
+			mv.addObject("value", value);
 			mv.setViewName("/views/modify.jsp");
 		}
-			
+
 		return mv;
 	}
-	
-	@RequestMapping(path="search.do", params="keyword", method=RequestMethod.GET) 
+
+	@RequestMapping(path = "search.do", params = "keyword", method = RequestMethod.GET)
 	public ModelAndView searchFilm(String keyword) {
 		ModelAndView mv = new ModelAndView();
 		List<Film> films = db.findFilmByKeyword(keyword);
-		if(films.size()==0) {
-			
+		if (films.size() == 0) {
+
 			mv.setViewName("/views/error.jsp");
-		}else {
-			mv.addObject("details",films);
+		} else {
+			mv.addObject("details", films);
 			mv.setViewName("/views/home.jsp");
 		}
 		return mv;
 	}
-	
-	@RequestMapping(path="changeFilm.do", method=RequestMethod.POST) 
+
+	@RequestMapping(path = "changeFilm.do", method = RequestMethod.POST)
 	public ModelAndView updateFilm(@ModelAttribute("film") @Valid Film film, Errors errors) {
 		ModelAndView mv = new ModelAndView();
-		
+
 //		System.out.println("film id is :" + film.getId());
-		if(errors.hasErrors()) {
+		if (errors.hasErrors()) {
 			mv.addObject("value", "add");
-		    mv.setViewName("/views/modify.jsp");
-		    return mv;
-		  }
-		
-		mv.addObject("details",db.updateFilm(film));
-		mv.setViewName("/views/result.jsp");
-		return mv;
+			mv.setViewName("/views/modify.jsp");
+			return mv;
+		} else {
+
+			mv.addObject("details", db.updateFilm(film));
+			mv.setViewName("/views/result.jsp");
+			return mv;
+		}
 	}
-	@RequestMapping(path="home.do", method=RequestMethod.GET) 
+
+	@RequestMapping(path = "home.do", method = RequestMethod.GET)
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView();
-		
+
 		mv.setViewName("/views/home.jsp");
 		return mv;
 	}
